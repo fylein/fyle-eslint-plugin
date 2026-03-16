238c2d9879be4b0551ee3675ccb76d4f4c269c15
Harshal Devmurari <harshal.devmurari@sage.com>
2026-03-16 23:59:45 +0530

    feat: docs and tests for no mutate format tokens (#15)
    * no mutate token docs
    
    * minor
    
    * feat: rule to restrict datapipe.transform with additional args (#16)
    
    * add new rule
    
    * comment fizes

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,9 +1,17 @@
 import i18nKeyNamingConvention from './rules/i18n-key-naming-convention.js';
+import noAngularCurrencyPipe from './rules/no-angular-currency-pipe.js';
+import noDatepipeTransformFormatArg from './rules/no-datepipe-transform-format-arg.js';
+import noInlineDateFormat from './rules/no-inline-date-format.js';
 import noHardcodedStrings from './rules/no-hardcoded-strings.js';
+import noMutateDisplayFormatTokens from './rules/no-mutate-format-tokens.js';
 
 const rules = {
   'i18n-key-naming-convention': i18nKeyNamingConvention,
+  'no-angular-currency-pipe': noAngularCurrencyPipe,
+  'no-datepipe-transform-format-arg': noDatepipeTransformFormatArg,
+  'no-inline-date-format': noInlineDateFormat,
   'no-hardcoded-strings': noHardcodedStrings,
+  'no-mutate-display-format-tokens': noMutateDisplayFormatTokens,
 };
 
 const configs = {
